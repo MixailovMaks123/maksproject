32491a09cdb4d36e1f13528e326dc0c3c2b9f1ca
MaksMixailov <mixailovmaks123@gmail.com>
2021-08-05 20:54:46 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+let arr = ['plum.png','orange.jpg','aple.bnp'];
+//console.log(arr[0]);
+
+//alert('Hello World!');
+
+//let answer = confirm("Are ypu here");
+//console.log(answer);
+
+//let answer = +prompt("Есть ли вам 18?","Yes");
+//console.log(typeof(answer));
+
+//console.log("arr" + " - object");
+//console.log(4 + +" - object");
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" ===2);
+
+let isChecked = true,
+    isClose = false;
+
+console.log(isChecked || isClose);
