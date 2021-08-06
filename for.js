05f53266c9f5ca72c6958e1f775b2c7b860a6388
MaksMixailov <mixailovmaks123@gmail.com>
2021-08-06 18:21:59 +0900

    задание

--- for.js
+++ for.js
@@ -1,1 +1,19 @@
-for.js
+let a = 50;
+
+// while(a < 55){
+//     console.log(a);
+//     a++;
+// }
+
+// do {
+//     console.log(a);
+//     a++; 
+// }
+// while(a<56);
+
+for(let i=1; i<55; i++ ){
+    if(i==10){
+        continue;
+    }
+    console.log(i);
+}
