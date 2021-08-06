14d505d08f9a57c7b933c6b450ae6182d1b431e3
MaksMixailov <mixailovmaks123@gmail.com>
2021-08-06 16:46:13 +0900

    add if, for

--- if.js
+++ if.js
@@ -1,1 +1,22 @@
-if.js
+/*let num = 50;
+
+if (num < 49){
+    console.log("неверно");
+}else if (num > 100){
+    console.log("много");
+}else{
+    console.log("Верно");
+}*/
+
+let second = 40;
+
+switch(second){
+    case second < 49:
+        console.log("Неверно");
+        break;
+    case 40:
+        console.log("Верно");
+        break;
+    default:
+        console.log("что-то не так");
+}
