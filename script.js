381992d1bbb5ec4246cf991722bdc0bdfd6761aa
MaksMixailov <mixailovmaks123@gmail.com>
2021-08-06 15:51:56 +0900

    js

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
-'use strict';
+let money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-let arr = ['plum.png','orange.jpg','aple.bnp'];
-//console.log(arr[0]);
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
+};
 
-//alert('Hello World!');
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
 
-//let answer = confirm("Are ypu here");
-//console.log(answer);
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
 
-//let answer = +prompt("Есть ли вам 18?","Yes");
-//console.log(typeof(answer));
-
-//console.log("arr" + " - object");
-//console.log(4 + +" - object");
-
-let incr = 10,
-    decr = 10;
-
-console.log(incr++);
-console.log(decr--);
-
-console.log(5%2);
-console.log("2" ===2);
-
-let isChecked = true,
-    isClose = false;
-
-console.log(isChecked || isClose);
+alert(appData.budget / 30);
