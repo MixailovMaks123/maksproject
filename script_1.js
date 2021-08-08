d8a0a93dbdf21a94be1a51b7d3abda36c1df3cf7
MaksMixailov <mixailovmaks123@gmail.com>
2021-08-09 00:50:56 +0900

    book Js if

--- script_1.js
+++ script_1.js
@@ -1,3 +1,37 @@
-"use strict";
+// "use strict";
 
-alert("я JavaScript!");
+// let a = "I JavaScript!";
+
+// alert(a);
+
+// let planetEarth = "Земля";
+
+// let currenVisitor;
+
+// currenVisitor = planetEarth;
+
+// // alert(currenVisitor);
+
+// alert (typeof currenVisitor);
+
+// let name = prompt("Ваше имя?", " ");
+
+// alert(`Ваше имя ${name}`);
+
+// let nameJS = prompt('официальное название JavaScript?', '');
+
+// if(nameJS == 'ECMAScript'){
+//     alert('Правильно!');
+// }else{
+//     alert('Вы не знаете?');
+// }
+
+let num = prompt('напишите число', '');
+
+if(num>0){
+    alert('1');
+}else if(num<0){
+    alert('-1');
+}else if(num==0){
+    alert('0');
+}
