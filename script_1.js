8c56a116efffe92129d8fc822d89a34d68d0e65f
MaksMixailov <mixailovmaks123@gmail.com>
2021-08-11 23:34:47 +0900

    JS ocnovi

--- script_1.js
+++ script_1.js
@@ -1,4 +1,4 @@
-// "use strict";
+"use strict";
 
 // let a = "I JavaScript!";
 
@@ -26,12 +26,119 @@
 //     alert('Вы не знаете?');
 // }
 
-let num = prompt('напишите число', '');
+// let num = prompt('напишите число', '');
 
-if(num>0){
-    alert('1');
-}else if(num<0){
-    alert('-1');
-}else if(num==0){
-    alert('0');
-}
+// if(num>0){
+//     alert('1');
+// }else if(num<0){
+//     alert('-1');
+// }else if(num==0){
+//     alert('0');
+// }
+
+// let name = prompt('кто там?', '');
+
+// if(name ==  "Админ"){
+    
+//     let password=prompt('пароль', '');
+    
+//     if(password=='Я главный'){
+//         alert('Здравствуйте!');
+//     }else if(password ==''||pass==null){
+//         alert('Отменено');
+//     }else{
+//         alert('Не верный пароль');
+//     }
+
+// }else if(name == '' || name == null){
+//     alert('Отменено');
+// }else{
+//     alert('Я вас не знаю');
+// }
+
+// for(let i=2; i<=10; ++i){
+//     if(i%2){
+//         continue;
+        
+//     }
+//     alert(i);
+// }
+
+
+// let i=0;
+// while(i<3){
+//     alert(`nuber ${i}!`);
+//     ++i;
+// }
+
+
+// let i;
+// do{
+//     i = prompt('Bведите число больше 100', 0);
+// }while(i<=100 && i);
+
+// let num = +prompt('напишите число', '');
+
+// let i = 2 + num;
+
+// switch (i) {
+//   case(3):
+//     alert( 'Маловато' );
+//     break;
+//   case(4):
+//     alert( 'В точку!' );
+//     break;
+//   case (5):
+//     alert( 'Перебор' );
+//     break;
+//   default:
+//     alert( "Нет таких значений" );
+// }
+
+
+// const number = +prompt('Введите число между 0 и 3', '');
+
+// switch(number){
+//     case(0):
+//     alert('Вы ввели число 0');
+//     break;
+
+//     case(1):
+//     alert('Вы ввели число 1');
+//     break;
+
+//     case(2):
+//     case(3):
+//     alert('Вы ввели число 2 или 3');
+//     break;
+
+//     default:
+//     alert('это другое число');
+// }
+
+
+// let age = +prompt('Ваш возраст','');
+
+// function sheckAge(age){
+//     (age>18)? true (alert('добро пожаловать')):
+//     confirm('Родители разрешили?');
+    
+// }
+
+// sheckAge(age);
+
+
+let agreement = confirm('Вы согластны?');
+
+let fun = (agreement)?
+    () => alert('Вы согласились.'):
+    () => alert('Вы отменили выполнение.');
+
+fun();
+
+
+
+
+
+
+
